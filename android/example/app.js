1f3f2f1107559d6541c284abf4ba097dc32caa6d
Mads Møller <mm@napp.dk>
2014-07-17 12:25:14 +0200

    sync the examples

--- android/example/app.js
+++ android/example/app.js
@@ -1,6 +1,5 @@
-
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+    backgroundColor: 'white'
 });
 win.open();
 
@@ -12,39 +11,38 @@ var NappJockey = require('dk.napp.jockey');
 NappJockey.appendUserAgent("my custom string");
 
 // webview
-var webview = NappJockey.createWebView({	
-	top: 70,
-	url:"http://yourwebsite.com/jockeyjs/index.html",
+var webview = NappJockey.createWebView({
+    top: 70,
+    url: "http://yourwebsite.com/jockeyjs/index.html",
 });
 
 
-webview.addEventListener('toggle-fullscreen',function(e) {
-	Ti.API.info( 'toggle-fullscreen' );
-    Ti.API.info( e );
-	Ti.API.info(JSON.stringify(e));
-	alert("You clicked : toggle-fullscreen");
+webview.addEventListener('toggle-fullscreen', function(e) {
+    Ti.API.info('toggle-fullscreen');
+    Ti.API.info(e);
+    Ti.API.info(JSON.stringify(e));
+    alert("You clicked : toggle-fullscreen");
 });
 
-webview.addEventListener('toggle-fullscreen-with-callback',function(e) {
-	Ti.API.info( 'toggle-fullscreen-with-callback' );
-    Ti.API.info( e );
-	Ti.API.info(JSON.stringify(e));
-	alert("You clicked : toggle-fullscreen-with-callback");
+webview.addEventListener('toggle-fullscreen-with-callback', function(e) {
+    Ti.API.info('toggle-fullscreen-with-callback');
+    Ti.API.info(e);
+    Ti.API.info(JSON.stringify(e));
+    alert("You clicked : toggle-fullscreen-with-callback");
 });
 
 win.add(webview);
 
 var btn = Ti.UI.createButton({
-	title:"Send event to webpage",
-	top:"20dp",
-	height:"40dp"
+    title: "Send event to webpage",
+    top: "20dp",
+    height: "40dp"
 });
-btn.addEventListener("click", function(){
-	
-	// send event with payload
-	webview.sendJockeyData("color-change", {
-		color: "#CB4437"
-	});
+btn.addEventListener("click", function() {
+
+    // send event with payload
+    webview.sendJockeyData("color-change", {
+        color: "#CB4437"
+    });
 });
 win.add(btn);
-
--- ios/example/app.js
+++ ios/example/app.js
@@ -1,42 +1,48 @@
-
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+    backgroundColor: 'white'
 });
 win.open();
 
+
+
 var NappJockey = require('dk.napp.jockey');
 
+// optinal set the user agent string before creating the webview.
 NappJockey.appendUserAgent("my custom string");
 
 // webview
-var webview = NappJockey.createWebView({	
-	top: 70,
-	debug:true,
-	//url:"http://www.whatsmyuseragent.com",
-	url:"http://angular.dev/test.html"
+var webview = NappJockey.createWebView({
+    top: 70,
+    url: "http://yourwebsite.com/jockeyjs/index.html",
 });
 
-webview.addEventListener('toggle-fullscreen',function(e) {
-	Ti.API.info( 'toggle-fullscreen' );
-    Ti.API.info( e );
+
+webview.addEventListener('toggle-fullscreen', function(e) {
+    Ti.API.info('toggle-fullscreen');
+    Ti.API.info(e);
+    Ti.API.info(JSON.stringify(e));
+    alert("You clicked : toggle-fullscreen");
 });
 
-webview.addEventListener('toggle-fullscreen-with-callback',function(e) {
-	Ti.API.info( 'toggle-fullscreen-with-callback' );
-    Ti.API.info( e );
+webview.addEventListener('toggle-fullscreen-with-callback', function(e) {
+    Ti.API.info('toggle-fullscreen-with-callback');
+    Ti.API.info(e);
+    Ti.API.info(JSON.stringify(e));
+    alert("You clicked : toggle-fullscreen-with-callback");
 });
 
 win.add(webview);
 
 var btn = Ti.UI.createButton({
-	title:"send event",
-	top:20,
-	height:"40dp"
+    title: "Send event to webpage",
+    top: "20dp",
+    height: "40dp"
 });
-btn.addEventListener("click", function(){
-	webview.sendJockeyData("color-change", {
-		color: "#333333"
-	});
+btn.addEventListener("click", function() {
+
+    // send event with payload
+    webview.sendJockeyData("color-change", {
+        color: "#CB4437"
+    });
 });
 win.add(btn);
-
